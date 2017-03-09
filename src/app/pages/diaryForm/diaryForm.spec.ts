29e46ba36220805baeb3dd15b10f000fedb3c1a3
Hemery <frederic.hemery@capgemini.com>
2017-03-09 11:36:32 +0100

    Fix date issue on diaryForm

--- src/app/pages/diaryForm/diaryForm.spec.ts
+++ src/app/pages/diaryForm/diaryForm.spec.ts
@@ -10,8 +10,6 @@ describe('Page: News Form', () => {
   let userServiceMock: any;
   let diaryServiceMock: any;
   let entry: DiaryEntry;
-  let d: Date = new Date();
-  let date: string;
   let type: string;
   let label: string;
 
@@ -22,7 +20,6 @@ describe('Page: News Form', () => {
     navController = jasmine.createSpyObj('navController', ['push']);
     altController = jasmine.createSpyObj('altController', ['present']);
     params = { data: { type: 'Medication' }, get: null };
-    date = d.getFullYear() + "-" + ("0"+(d.getMonth()+1)).slice(-2) + "-" + ("0" + d.getDate()).slice(-2);
     type = params.data.type;
     entry = new DiaryEntry ('', this.type, '', this.date, '1');
     label = 'md-analytics';
@@ -33,9 +30,6 @@ describe('Page: News Form', () => {
     it('should initialize the error indicator to false', () => {
       expect(component.errorOccurred).toBe(false);
     });
-    it('should initialize the date to sysdate', () => {
-      expect(component.date).toBe(date);
-    });
     it('should initialize the label to empty', () => {
       expect(component.labelD).toEqual(label);
     });
@@ -45,7 +39,9 @@ describe('Page: News Form', () => {
   });
 
   describe('onSubmit', () => {
-     
+    xit('new entry should have correct date', () =>{
+
+    });
   });
 
 });
--- src/app/pages/diaryForm/diaryForm.ts
+++ src/app/pages/diaryForm/diaryForm.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, AlertController, NavParams } from 'ionic-angular';
 import { DiaryEntry
-  //, SymptomCheck, Medication, Activity, FreeEntry 
+  //, SymptomCheck, Medication, Activity, FreeEntry
 } from '../../model/_model';
 import { UserService, DiaryService } from '../../services/services';
 
@@ -12,8 +12,6 @@ import { UserService, DiaryService } from '../../services/services';
 })
 
 export class DiaryForm {
-  public d: Date = new Date();
-  public date: string = this.d.getTime().toString();
   public entry: DiaryEntry;
   /*public activity: Activity = null;
   public freeEntry: FreeEntry = null;
@@ -26,7 +24,7 @@ export class DiaryForm {
   constructor(public params: NavParams, public alertCtrl: AlertController, private diaryService: DiaryService,
     public navCtrl: NavController, public userService: UserService) {
     this.type = params.data.type;
-    this.entry = new DiaryEntry('', this.type, '', this.date, 'a123875114-bf258314');
+    this.entry = new DiaryEntry('', this.type, '', (new Date()).getTime().toString(), 'a123875114-bf258314');
     if (this.type == 'SymptomCheck') {
       this.labelD = 'md-american-football';
      // this.entry = new DiaryEntry('', this.type, '', this.date, 'a123875114-bf258314');
